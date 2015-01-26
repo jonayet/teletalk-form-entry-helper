05680d9f0d9e6d9db399fdcf9afdc196e33684c5
Jonayet Hossain <jonayet.bu@gmail.com>
2015-01-26 02:58:20 -0800

    updated

---
+++ FormEntryHelper/FormEntryHelper/DataEntryHelperUiForm.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace FormEntryHelper
+{
+    partial class DataEntryHelperUiForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.selectFolderButton = new System.Windows.Forms.Button();
+            this.formViewPictureBox = new System.Windows.Forms.PictureBox();
+            this.viewPanel = new System.Windows.Forms.Panel();
+            this.saveAndNextButton = new System.Windows.Forms.Button();
+            this.mobileNoTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.infoLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.formViewPictureBox)).BeginInit();
+            this.viewPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // selectFolderButton
+            // 
+            this.selectFolderButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.selectFolderButton.Location = new System.Drawing.Point(719, 11);
+            this.selectFolderButton.Name = "selectFolderButton";
+            this.selectFolderButton.Size = new System.Drawing.Size(80, 31);
+            this.selectFolderButton.TabIndex = 0;
+            this.selectFolderButton.Text = "Load";
+            this.selectFolderButton.UseVisualStyleBackColor = true;
+            this.selectFolderButton.Click += new System.EventHandler(this.selectFolderButton_Click);
+            // 
+            // formViewPictureBox
+            // 
+            this.formViewPictureBox.Location = new System.Drawing.Point(3, 3);
+            this.formViewPictureBox.Name = "formViewPictureBox";
+            this.formViewPictureBox.Size = new System.Drawing.Size(100, 50);
+            this.formViewPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.formViewPictureBox.TabIndex = 1;
+            this.formViewPictureBox.TabStop = false;
+            // 
+            // viewPanel
+            // 
+            this.viewPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.viewPanel.AutoScroll = true;
+            this.viewPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.viewPanel.Controls.Add(this.formViewPictureBox);
+            this.viewPanel.Location = new System.Drawing.Point(17, 52);
+            this.viewPanel.Name = "viewPanel";
+            this.viewPanel.Size = new System.Drawing.Size(422, 336);
+            this.viewPanel.TabIndex = 2;
+            // 
+            // saveAndNextButton
+            // 
+            this.saveAndNextButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.saveAndNextButton.Location = new System.Drawing.Point(333, 404);
+            this.saveAndNextButton.Name = "saveAndNextButton";
+            this.saveAndNextButton.Size = new System.Drawing.Size(106, 32);
+            this.saveAndNextButton.TabIndex = 3;
+            this.saveAndNextButton.Text = "Save && Go Next!";
+            this.saveAndNextButton.UseVisualStyleBackColor = true;
+            this.saveAndNextButton.Click += new System.EventHandler(this.saveAndNextButton_Click);
+            // 
+            // mobileNoTextBox
+            // 
+            this.mobileNoTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.mobileNoTextBox.Location = new System.Drawing.Point(227, 410);
+            this.mobileNoTextBox.Name = "mobileNoTextBox";
+            this.mobileNoTextBox.Size = new System.Drawing.Size(100, 20);
+            this.mobileNoTextBox.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(163, 413);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Mobile No:";
+            // 
+            // infoLabel
+            // 
+            this.infoLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.infoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.infoLabel.ForeColor = System.Drawing.Color.Red;
+            this.infoLabel.Location = new System.Drawing.Point(12, 9);
+            this.infoLabel.Name = "infoLabel";
+            this.infoLabel.Size = new System.Drawing.Size(284, 40);
+            this.infoLabel.TabIndex = 6;
+            this.infoLabel.Text = "Folder Not Selected";
+            this.infoLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // DataEntryHelperUiForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(814, 448);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.infoLabel);
+            this.Controls.Add(this.mobileNoTextBox);
+            this.Controls.Add(this.viewPanel);
+            this.Controls.Add(this.saveAndNextButton);
+            this.Controls.Add(this.selectFolderButton);
+            this.Name = "DataEntryHelperUiForm";
+            this.Text = "DataEntryHelperUiForm";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.formViewPictureBox)).EndInit();
+            this.viewPanel.ResumeLayout(false);
+            this.viewPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button selectFolderButton;
+        private System.Windows.Forms.PictureBox formViewPictureBox;
+        private System.Windows.Forms.Panel viewPanel;
+        private System.Windows.Forms.Button saveAndNextButton;
+        private System.Windows.Forms.TextBox mobileNoTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label infoLabel;
+    }
+}
+
---
+++ FormEntryHelper/FormEntryHelper/DataEntryHelperUiForm.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FormEntryHelper
+{
+    public partial class DataEntryHelperUiForm : Form
+    {
+        private static readonly string[] FileFilters = { "*.jpg", "*.bmp" };
+        private int _fileIndex;
+        private List<string> _files;
+        private const double ZoomScale = 2;
+        private const int InfoDelay = 1000;
+        private readonly Timer _infoTimer;
+
+        public DataEntryHelperUiForm()
+        {
+            InitializeComponent();
+            _infoTimer = new Timer() { Enabled = false, Interval = InfoDelay };
+            _infoTimer.Tick += new EventHandler(infoTimer_Tick);
+        }
+
+        private void selectFolderButton_Click(object sender, EventArgs e)
+        {
+            var folderBrowser = new FolderBrowserDialog();
+            var result = folderBrowser.ShowDialog();
+            if(result != DialogResult.OK) { return; }
+
+            
+            _files = new List<string>();
+            foreach (string filter in FileFilters)
+            {
+                _files.AddRange(Directory.GetFiles(folderBrowser.SelectedPath, filter));
+            }
+            _files.Sort();
+            _fileIndex = 0;
+
+            // show first form
+            if (_files.Count > 0)
+            {
+                ViewFormImage(_files[_fileIndex]);
+                ShowSuccessMessage("Folder Selected");
+            }
+            else
+            {
+                ShowErrorMessage("Folder Not Selected");
+            }
+        }
+
+        private void saveAndNextButton_Click(object sender, EventArgs e)
+        {
+            if (_files == null)
+            {
+                ShowErrorMessage("Folder Not Selected");
+                return;
+            }
+
+            if (_files.Count == 0)
+            {
+                ShowErrorMessage("No file");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(mobileNoTextBox.Text))
+            {
+                ShowErrorMessage("Write Mobile Number");
+                return;
+            }
+
+            if (_files.Count > (_fileIndex + 2))
+            {
+                _fileIndex += 2;
+                ViewFormImage(_files[_fileIndex]);
+            }
+        }
+
+        void ViewFormImage(string imagePath)
+        {
+            using (var originalImage = new Bitmap(imagePath))
+            using (var scaledImage = new Bitmap((int) (originalImage.Width * ZoomScale), (int) (originalImage.Height * ZoomScale)))
+            using (var gfx = Graphics.FromImage(scaledImage))
+            {
+                gfx.DrawImage(originalImage, 0, 0, scaledImage.Width, scaledImage.Height);
+                formViewPictureBox.Image = (Bitmap)scaledImage.Clone();
+                viewPanel.VerticalScroll.Value = 0;
+                viewPanel.HorizontalScroll.Value = viewPanel.HorizontalScroll.Maximum - 50;
+                viewPanel.Invalidate();
+            }
+        }
+
+        void ShowSuccessMessage(string msg)
+        {
+            infoLabel.Text = msg;
+            infoLabel.ForeColor = Color.MediumSeaGreen;
+            infoLabel.Visible = true;
+            _infoTimer.Enabled = true;
+        }
+
+        void ShowWarningMessage(string msg)
+        {
+            infoLabel.Text = msg;
+            infoLabel.ForeColor = Color.Orange;
+            infoLabel.Visible = true;
+            _infoTimer.Enabled = true;
+        }
+
+        void ShowErrorMessage(string msg)
+        {
+            infoLabel.Text = msg;
+            infoLabel.ForeColor = Color.Red;
+            infoLabel.Visible = true;
+            _infoTimer.Enabled = true;
+        }
+
+        void infoTimer_Tick(object sender, EventArgs e)
+        {
+            infoLabel.Visible = false;
+            _infoTimer.Enabled = false;
+        }
+    }
+}
