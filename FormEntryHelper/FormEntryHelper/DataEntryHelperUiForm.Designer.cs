880d38c019737f1d00daa1f41bd1143ca21bfb6f
Jonayet Hossain <jonayet.bu@gmail.com>
2015-01-27 02:29:18 -0800

    updated

--- FormEntryHelper/FormEntryHelper/DataEntryHelperUiForm.Designer.cs
+++ FormEntryHelper/FormEntryHelper/DataEntryHelperUiForm.Designer.cs
@@ -28,24 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DataEntryHelperUiForm));
             this.selectFolderButton = new System.Windows.Forms.Button();
             this.formViewPictureBox = new System.Windows.Forms.PictureBox();
-            this.viewPanel = new System.Windows.Forms.Panel();
+            this.formViewPanel = new System.Windows.Forms.Panel();
             this.saveAndNextButton = new System.Windows.Forms.Button();
-            this.mobileNoTextBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.infoLabel = new System.Windows.Forms.Label();
+            this.idViewPanel = new System.Windows.Forms.Panel();
+            this.idViewPictureBox = new System.Windows.Forms.PictureBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.mobileNoMaskedTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.skipButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.formViewPictureBox)).BeginInit();
-            this.viewPanel.SuspendLayout();
+            this.formViewPanel.SuspendLayout();
+            this.idViewPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.idViewPictureBox)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // selectFolderButton
             // 
             this.selectFolderButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.selectFolderButton.Location = new System.Drawing.Point(719, 11);
+            this.selectFolderButton.Location = new System.Drawing.Point(680, 9);
             this.selectFolderButton.Name = "selectFolderButton";
-            this.selectFolderButton.Size = new System.Drawing.Size(80, 31);
+            this.selectFolderButton.Size = new System.Drawing.Size(119, 31);
             this.selectFolderButton.TabIndex = 0;
+            this.selectFolderButton.TabStop = false;
             this.selectFolderButton.Text = "Load";
             this.selectFolderButton.UseVisualStyleBackColor = true;
             this.selectFolderButton.Click += new System.EventHandler(this.selectFolderButton_Click);
@@ -54,48 +65,38 @@
             // 
             this.formViewPictureBox.Location = new System.Drawing.Point(3, 3);
             this.formViewPictureBox.Name = "formViewPictureBox";
-            this.formViewPictureBox.Size = new System.Drawing.Size(100, 50);
+            this.formViewPictureBox.Size = new System.Drawing.Size(391, 344);
             this.formViewPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.formViewPictureBox.TabIndex = 1;
             this.formViewPictureBox.TabStop = false;
             // 
-            // viewPanel
+            // formViewPanel
             // 
-            this.viewPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.viewPanel.AutoScroll = true;
-            this.viewPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.viewPanel.Controls.Add(this.formViewPictureBox);
-            this.viewPanel.Location = new System.Drawing.Point(17, 52);
-            this.viewPanel.Name = "viewPanel";
-            this.viewPanel.Size = new System.Drawing.Size(422, 336);
-            this.viewPanel.TabIndex = 2;
+            this.formViewPanel.AutoScroll = true;
+            this.formViewPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.formViewPanel.Controls.Add(this.formViewPictureBox);
+            this.formViewPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.formViewPanel.Location = new System.Drawing.Point(0, 0);
+            this.formViewPanel.Name = "formViewPanel";
+            this.formViewPanel.Size = new System.Drawing.Size(397, 350);
+            this.formViewPanel.TabIndex = 2;
             // 
             // saveAndNextButton
             // 
-            this.saveAndNextButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.saveAndNextButton.Location = new System.Drawing.Point(333, 404);
+            this.saveAndNextButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.saveAndNextButton.Location = new System.Drawing.Point(303, 404);
             this.saveAndNextButton.Name = "saveAndNextButton";
             this.saveAndNextButton.Size = new System.Drawing.Size(106, 32);
-            this.saveAndNextButton.TabIndex = 3;
+            this.saveAndNextButton.TabIndex = 1;
             this.saveAndNextButton.Text = "Save && Go Next!";
             this.saveAndNextButton.UseVisualStyleBackColor = true;
             this.saveAndNextButton.Click += new System.EventHandler(this.saveAndNextButton_Click);
             // 
-            // mobileNoTextBox
-            // 
-            this.mobileNoTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.mobileNoTextBox.Location = new System.Drawing.Point(227, 410);
-            this.mobileNoTextBox.Name = "mobileNoTextBox";
-            this.mobileNoTextBox.Size = new System.Drawing.Size(100, 20);
-            this.mobileNoTextBox.TabIndex = 4;
-            // 
             // label1
             // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(163, 413);
+            this.label1.Location = new System.Drawing.Point(94, 413);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(58, 13);
             this.label1.TabIndex = 5;
@@ -103,33 +104,105 @@
             // 
             // infoLabel
             // 
-            this.infoLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.infoLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.infoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.infoLabel.ForeColor = System.Drawing.Color.Red;
             this.infoLabel.Location = new System.Drawing.Point(12, 9);
             this.infoLabel.Name = "infoLabel";
-            this.infoLabel.Size = new System.Drawing.Size(284, 40);
+            this.infoLabel.Size = new System.Drawing.Size(662, 31);
             this.infoLabel.TabIndex = 6;
             this.infoLabel.Text = "Folder Not Selected";
             this.infoLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // idViewPanel
+            // 
+            this.idViewPanel.AutoScroll = true;
+            this.idViewPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.idViewPanel.Controls.Add(this.idViewPictureBox);
+            this.idViewPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.idViewPanel.Location = new System.Drawing.Point(0, 0);
+            this.idViewPanel.Name = "idViewPanel";
+            this.idViewPanel.Size = new System.Drawing.Size(386, 350);
+            this.idViewPanel.TabIndex = 7;
+            // 
+            // idViewPictureBox
+            // 
+            this.idViewPictureBox.Location = new System.Drawing.Point(3, 3);
+            this.idViewPictureBox.Name = "idViewPictureBox";
+            this.idViewPictureBox.Size = new System.Drawing.Size(380, 344);
+            this.idViewPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.idViewPictureBox.TabIndex = 1;
+            this.idViewPictureBox.TabStop = false;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(12, 48);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.formViewPanel);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.idViewPanel);
+            this.splitContainer1.Size = new System.Drawing.Size(787, 350);
+            this.splitContainer1.SplitterDistance = 397;
+            this.splitContainer1.TabIndex = 8;
+            this.splitContainer1.TabStop = false;
+            // 
+            // mobileNoMaskedTextBox
+            // 
+            this.mobileNoMaskedTextBox.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.mobileNoMaskedTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mobileNoMaskedTextBox.Location = new System.Drawing.Point(158, 406);
+            this.mobileNoMaskedTextBox.Mask = "\\01500000000";
+            this.mobileNoMaskedTextBox.Name = "mobileNoMaskedTextBox";
+            this.mobileNoMaskedTextBox.Size = new System.Drawing.Size(130, 26);
+            this.mobileNoMaskedTextBox.TabIndex = 0;
+            // 
+            // skipButton
+            // 
+            this.skipButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.skipButton.Location = new System.Drawing.Point(555, 9);
+            this.skipButton.Name = "skipButton";
+            this.skipButton.Size = new System.Drawing.Size(119, 31);
+            this.skipButton.TabIndex = 10;
+            this.skipButton.TabStop = false;
+            this.skipButton.Text = "Skip";
+            this.skipButton.UseVisualStyleBackColor = true;
+            this.skipButton.Click += new System.EventHandler(this.skipButton_Click);
+            // 
             // DataEntryHelperUiForm
             // 
+            this.AcceptButton = this.saveAndNextButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(814, 448);
+            this.Controls.Add(this.skipButton);
+            this.Controls.Add(this.mobileNoMaskedTextBox);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.infoLabel);
-            this.Controls.Add(this.mobileNoTextBox);
-            this.Controls.Add(this.viewPanel);
             this.Controls.Add(this.saveAndNextButton);
             this.Controls.Add(this.selectFolderButton);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "DataEntryHelperUiForm";
             this.Text = "Data Entry Helper";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             ((System.ComponentModel.ISupportInitialize)(this.formViewPictureBox)).EndInit();
-            this.viewPanel.ResumeLayout(false);
-            this.viewPanel.PerformLayout();
+            this.formViewPanel.ResumeLayout(false);
+            this.formViewPanel.PerformLayout();
+            this.idViewPanel.ResumeLayout(false);
+            this.idViewPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.idViewPictureBox)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -139,11 +212,15 @@
 
         private System.Windows.Forms.Button selectFolderButton;
         private System.Windows.Forms.PictureBox formViewPictureBox;
-        private System.Windows.Forms.Panel viewPanel;
+        private System.Windows.Forms.Panel formViewPanel;
         private System.Windows.Forms.Button saveAndNextButton;
-        private System.Windows.Forms.TextBox mobileNoTextBox;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label infoLabel;
+        private System.Windows.Forms.Panel idViewPanel;
+        private System.Windows.Forms.PictureBox idViewPictureBox;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.MaskedTextBox mobileNoMaskedTextBox;
+        private System.Windows.Forms.Button skipButton;
     }
 }
 
--- FormEntryHelper/FormEntryHelper/DataEntryHelperUiForm.cs
+++ FormEntryHelper/FormEntryHelper/DataEntryHelperUiForm.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows.Forms;
 
@@ -11,13 +13,16 @@ namespace FormEntryHelper
     public partial class DataEntryHelperUiForm : Form
     {
         private static readonly string[] FileFilters = { "*.jpg", "*.bmp" };
-        private int _fileIndex;
-        private Stack<string> _files;
+        private Queue<string> _filesInFolder;
         private const double ZoomScale = 2;
         private const int InfoDelay = 1000;
         private const string OutputFolder = "Output";
+        private const string IdSufix = "_ID";
         private readonly Timer _infoTimer;
         private string _selectedFolderPath = "";
+        private string _selectedFormPath = "";
+        private string _selectedIdPath = "";
+        private int _fileLeft = 0;
 
         public DataEntryHelperUiForm()
         {
@@ -40,12 +45,14 @@ namespace FormEntryHelper
             }
 
             filePaths.Sort();
-            foreach (var filePath in filePaths) { _files.Push(filePath); }
+            _filesInFolder = new Queue<string>();
+            foreach (var filePath in filePaths) { _filesInFolder.Enqueue(filePath); }
 
             // show first form
-            if (_files.Count > 0)
+            if (_filesInFolder.Count > 0)
             {
-                ViewFormImage(_files.Peek());
+                SelectNextFormAndId();
+                ShowSelectedFormAndImage(_selectedFormPath, _selectedIdPath);
                 ShowSuccessMessage("Folder Selected");
                 if (!Directory.Exists(Path.Combine(_selectedFolderPath, OutputFolder)))
                 {
@@ -58,50 +65,104 @@ namespace FormEntryHelper
             }
         }
 
-        private void saveAndNextButton_Click(object sender, EventArgs e)
+        void SelectNextFormAndId()
         {
-            if (_files == null)
+            _fileLeft = _filesInFolder.Count;
+            try
             {
-                ShowErrorMessage("Folder Not Selected");
-                return;
+                _selectedFormPath = _filesInFolder.Dequeue();
+            }
+            catch
+            {
+                _selectedFormPath = "";
             }
 
-            if (_files.Count == 0)
+            try
             {
-                ShowErrorMessage("No file");
-                return;
+                _selectedIdPath = _filesInFolder.Dequeue();
             }
-
-            if (string.IsNullOrEmpty(mobileNoTextBox.Text))
+            catch
             {
-                ShowErrorMessage("Write Mobile Number");
-                return;
-            }
-
-            string selectedFile = _files.Pop();
-
-            //mobileNoTextBox.Text + Path.GetExtension(_files[_fileIndex])
-            File.Move(selectedFile, Path.Combine(_selectedFolderPath, OutputFolder) + @"\" + Path.GetFileName(selectedFile));
-            if (_files.Count > (_fileIndex + 2))
-            {
-                ViewFormImage(selectedFile);
-                mobileNoTextBox.Text = "";
+                _selectedIdPath = "";
             }
         }
 
-        void ViewFormImage(string imagePath)
+        private void saveAndNextButton_Click(object sender, EventArgs e)
         {
-            Text = Path.GetFileNameWithoutExtension(imagePath);
-            using (var originalImage = new Bitmap(imagePath))
-            using (var scaledImage = new Bitmap((int) (originalImage.Width * ZoomScale), (int) (originalImage.Height * ZoomScale)))
-            using (var gfx = Graphics.FromImage(scaledImage))
+            if (_filesInFolder == null) { ShowErrorMessage("Folder Not Selected"); return; }
+            if (mobileNoMaskedTextBox.Text.Length < 11) { ShowErrorMessage("Incorrect Mobile Number"); return; }
+            if (string.IsNullOrEmpty(_selectedFormPath) && string.IsNullOrEmpty(_selectedFormPath)) { ShowErrorMessage("No file"); return; }
+            if (string.IsNullOrEmpty(_selectedFormPath)) { ShowErrorMessage("Form is missing"); } 
+            else if (string.IsNullOrEmpty(_selectedIdPath)) { ShowErrorMessage("Id is missing"); }
+
+            string newFormPath = Path.Combine(_selectedFolderPath, OutputFolder) + @"\" + mobileNoMaskedTextBox.Text + Path.GetExtension(_selectedFormPath);
+            string newIdPath = Path.Combine(_selectedFolderPath, OutputFolder) + @"\" + mobileNoMaskedTextBox.Text + IdSufix + Path.GetExtension(_selectedIdPath);
+            if (File.Exists(newFormPath) && File.Exists(newIdPath)) { ShowErrorMessage("Form & ID both exist"); return; }
+            if (!File.Exists(newFormPath)) { File.Move(_selectedFormPath, newFormPath); } else { ShowErrorMessage("Form exist"); }
+            if (!File.Exists(newIdPath)) { File.Move(_selectedIdPath, newIdPath); } else { ShowErrorMessage("Id exist"); }
+
+            ShowSuccessMessage("Success");
+            mobileNoMaskedTextBox.Text = "015";
+            mobileNoMaskedTextBox.ScrollToCaret();
+            mobileNoMaskedTextBox.Focus();
+
+            SelectNextFormAndId();
+            ShowSelectedFormAndImage(_selectedFormPath, _selectedIdPath);
+        }
+
+        private void ShowSelectedFormAndImage(string formImagePath, string idImagePath)
+        {
+            try
             {
-                gfx.DrawImage(originalImage, 0, 0, scaledImage.Width, scaledImage.Height);
-                formViewPictureBox.Image = (Bitmap)scaledImage.Clone();
-                viewPanel.VerticalScroll.Value = 0;
-                viewPanel.HorizontalScroll.Value = viewPanel.HorizontalScroll.Maximum - 50;
-                viewPanel.Invalidate();
+                Text = !string.IsNullOrEmpty(formImagePath) ? Path.GetFileNameWithoutExtension(formImagePath) + " - " + _fileLeft : "";
+                using (var originalImage = new Bitmap(formImagePath))
+                using (var scaledImage = new Bitmap((int) (originalImage.Width*ZoomScale), (int) (originalImage.Height*ZoomScale)))
+                using (var gfx = Graphics.FromImage(scaledImage))
+                {
+                    gfx.DrawImage(originalImage, 0, 0, scaledImage.Width, scaledImage.Height);
+                    var lastImage = formViewPictureBox.Image;
+                    formViewPictureBox.Image = (Bitmap) scaledImage.Clone();
+                    if (lastImage != null) { lastImage.Dispose(); }
+                    formViewPanel.VerticalScroll.Value = 0;
+                    formViewPanel.HorizontalScroll.Value = formViewPanel.HorizontalScroll.Maximum - 50;
+                    formViewPanel.Invalidate();
+                }
             }
+            catch { }
+
+            try
+            {
+                using (var originalImage = new Bitmap(idImagePath))
+                using (var scaledImage = new Bitmap(originalImage.Width, originalImage.Height))
+                using (var gfx = Graphics.FromImage(scaledImage))
+                {
+                    gfx.DrawImage(originalImage, 0, 0, scaledImage.Width, scaledImage.Height);
+                    var lastImage = idViewPictureBox.Image;
+                    idViewPictureBox.Image = (Bitmap) scaledImage.Clone();
+                    if (lastImage != null) { lastImage.Dispose(); }
+                    idViewPanel.VerticalScroll.Value = 0;
+                    idViewPanel.HorizontalScroll.Value = idViewPanel.HorizontalScroll.Maximum - 50;
+                    idViewPanel.Invalidate();
+                }
+            }
+            catch { }
+        }
+
+        private void skipButton_Click(object sender, EventArgs e)
+        {
+            if (_filesInFolder == null) { ShowErrorMessage("Folder Not Selected"); return; }
+            var res = MessageBox.Show("Are you sure to skip?", "Warning!", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if(res != DialogResult.Yes) { return; }
+
+            try
+            {
+                _selectedFormPath = _selectedIdPath;
+                _selectedIdPath =_filesInFolder.Dequeue();
+                _fileLeft = _filesInFolder.Count;
+                ShowSelectedFormAndImage(_selectedFormPath, _selectedIdPath);
+                ShowSuccessMessage("Skipped");
+            }
+            catch{}
         }
 
         void ShowSuccessMessage(string msg)
